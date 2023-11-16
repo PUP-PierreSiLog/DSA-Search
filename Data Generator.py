59505defa2f80637b09678bb251716466b1d2b57
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-11-16 16:48:39 +0800

    Revised, made every integer unique

--- Data Generator.py
+++ Data Generator.py
@@ -4,8 +4,8 @@ import random
 lower_limit = 1
 upper_limit = 10000
 
-# Generate 100 random integers and store them in a list
-random_integers = [random.randint(lower_limit, upper_limit) for _ in range(10000)]
+# Generate 100 unique random integers and store them in a list
+random_integers = random.sample(range(lower_limit, upper_limit + 1), 10000)
 
 # Print the generated random integers
 print(random_integers)
