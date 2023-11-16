fb963affaee86fcc23bb8fa647bb5102e0df962a
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-11-16 15:19:15 +0800

    Data Sheets

--- Data Generator.py
+++ Data Generator.py
@@ -2,10 +2,10 @@ import random
 
 # Set the range for random integers
 lower_limit = 1
-upper_limit = 100
+upper_limit = 10000
 
 # Generate 100 random integers and store them in a list
-random_integers = [random.randint(lower_limit, upper_limit) for _ in range(100)]
+random_integers = [random.randint(lower_limit, upper_limit) for _ in range(10000)]
 
 # Print the generated random integers
 print(random_integers)
